91c774b192b24e692703afa92149f121dc87f5d0
dogasarpsezer-dalak <sarp@dalakgames.com>
2022-04-29 20:25:56 +0300

    Bayram Commit

--- DalakGITApp/DalakGITConsole/AutoBuildProgram.cs
+++ DalakGITApp/DalakGITConsole/AutoBuildProgram.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using  System.Net.;
+using System.Text.RegularExpressions;
 using System.Timers;
 
 namespace DalakGITConsole
@@ -13,29 +14,45 @@ namespace DalakGITConsole
         public static Timer commitGetTimer = new Timer(3000);
         public static string oldCommitHash = "";
         public static string pathOfRepo = "";
+        public static Queue<string[]> repoBuildQueue = new Queue<string[]>();  
         public static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO AUTOMATION BUILD APP THAT GAVE THE INTERN ONLY A LOT OF PAIN :) PRESS ANY KEY TO ABORT");
-            Console.WriteLine("Enter the repo path");
-            pathOfRepo = Console.ReadLine();
+            
+            InitBuildRepo();
+            
+            /*pathOfRepo = Console.ReadLine();
             commitGetTimer.Enabled = true;
             commitGetTimer.Elapsed += TriggerGIT;
             commitGetTimer.AutoReset = true;
             commitGetTimer.Start();
-            Console.ReadKey();
+            Console.ReadKey();*/
         }
 
-        public static void TriggerGITHUBWindows(Object sender, ElapsedEventArgs e)
+        public static void InitBuildRepo()
         {
-            
+            var path = Directory.GetCurrentDirectory();
+            Regex buildDirectoryRegex = new Regex(".+pipeline");
+            Regex repoURLRegex = new Regex(".+git");
+            Regex repoBuildStatusRegex = new Regex("[|].+[E,e][|]");
+            pathOfRepo = buildDirectoryRegex.Match(path).Value;
+
+            StreamReader streamReader = new StreamReader(pathOfRepo + "\\build_queue.txt");
+            while (!streamReader.EndOfStream)
+            {
+                var repoString = streamReader.ReadLine();
+                string[] repoStringPieces = new string[2];
+                repoStringPieces[0] = repoURLRegex.Match(repoString).Value;
+                repoStringPieces[1] = repoBuildStatusRegex.Match(repoString).Value;
+                Console.WriteLine(repoStringPieces[1]);
+                repoBuildQueue.Enqueue(repoStringPieces);
+            }
         }
-        
         public static void TriggerGIT(Object sender,ElapsedEventArgs e)
         {
-            string path =  pathOfRepo;
-            string pathRepo = path.Replace(@"\","/");
-            string gitLogCommand = string.Format(" --git-dir={0}/.git --work-tree={0} log -1 --pretty=format:\"%H\"",pathRepo);
-            string gitPullCommand = string.Format(" --git-dir={0}/.git --work-tree={0} pull ",pathRepo);;
+            string path = pathOfRepo.Replace(@"\","/");
+            string gitLogCommand = string.Format(" --git-dir={0}/.git --work-tree={0} log -1 --pretty=format:\"%H\"",path);
+            string gitPullCommand = string.Format(" --git-dir={0}/.git --work-tree={0} pull ",path);
             
             ProcessStartInfo processStartInfo = new ProcessStartInfo()
             {
@@ -48,13 +65,11 @@ namespace DalakGITConsole
                 //Application to start
                 FileName = "git.exe",
                 
-                Arguments = gitPullCommand
+                Arguments = gitLogCommand
             };
             
             Process gitProcess = new Process();
             gitProcess.StartInfo = processStartInfo;
-            gitProcess.Start();
-
             processStartInfo.Arguments = gitLogCommand;
             gitProcess.Start();
             string output = gitProcess.StandardOutput.ReadToEnd();
@@ -63,7 +78,10 @@ namespace DalakGITConsole
             if (!string.Equals(output, oldCommitHash))
             {
                 oldCommitHash = output;
-                Console.WriteLine(output);
+                processStartInfo.Arguments = gitPullCommand;
+                gitProcess.Start();
+                gitProcess.WaitForExit();
+                //Trigger Queueya al
             }
         }
     }
